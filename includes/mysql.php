a052622dcfc529368d14372c080a765ebd74dd2a
yangLiu57670838 <yang.liu9@griffithuni.edu.au>
2015-03-04 18:13:28 +1100

    Delete mysql.php

--- includes/mysql.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("HOST", "differentgifts2.db.10140891.hostedresource.com");
-define("USER", "differentgifts2");
-define("PASSWORD", "a1B2c3d4@");
-define("DATABASE", "differentgifts2");
-?>
